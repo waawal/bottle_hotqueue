6bb30dba009d81a343859b8e8cd8912919504bb3
waawal <waawal@boom.ws>
2012-04-06 17:43:33 +0400

    Pushing version to 0.2

--- setup.py
+++ setup.py
@@ -7,11 +7,15 @@ try:
 except ImportError:
     from distutils.command.build_py import build_py
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(
     name = 'bottle-hotqueue',
     version = '0.2.0',
     url = 'https://github.com/waawal/bottle_hotqueue',
     description = 'FIFO Queue for Bottle built upon HotQueue',
+    long_description = long_description,
     author = 'Waawal',
     author_email = 'waawal@boom.ws',
     license = 'MIT',
@@ -24,6 +28,7 @@ setup(
         'hotqueue'
     ],
     classifiers = [
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -32,5 +37,4 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    cmdclass = {'build_py': build_py}
 )
